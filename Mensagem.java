46f807bcc48dd79e1070e05144741272b1065dc5
JoaoRLS <202403265249@alunos.estacio.br>
2024-06-25 05:34:07 -0300

    Start of the software project

--- Mensagem.java
+++ Mensagem.java
@@ -3,10 +3,13 @@ public class Mensagem{
 public static void main(String[] args){
 int x;
 int y;
+float w;
 
-y = 2+3;
-x = (2 + 1)/1*2;
-System.out.print("Olá Mundo!!!" + x);
+y = 2*1;
+x = 2+1;
+w = 5/2;
+
+System.out.println(w);
 
 }
 
